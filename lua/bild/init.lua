858937ae9644a883bce9a58cb9c403480414652a
keks137 <guntzjonas@gmail.com>
2025-01-23 22:11:42 +0100

    first real test

--- lua/bild/init.lua
+++ lua/bild/init.lua
@@ -4,34 +4,30 @@ function M.build()
 	vim.system({ "ls" }, { text = true })
 	print("buildy build")
 	-- Execute the 'ls' command
-	local handle = io.popen("ls -l") -- You can customize the command here
+	local handle = io.popen("make") -- You can customize the command here
+	local result = handle:read("*a") -- Read the output
+
+	if result == nil then
+		print("whoopsie")
+	end
+	handle:close()
+
 	if handle == nil then
 		print("whoopsie")
-	else
-		local result = handle:read("*a") -- Read the output
-
-		if result == nil then
-			print("whoopsie")
-		end
-		handle:close()
-
-		if handle == nil then
-			print("whoopsie")
-		end
-		-- Create a new buffer and set it to display the output
-		local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
-		vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result, "\n")) -- Set the buffer lines
-
-		-- Open a new window with the buffer
-		vim.api.nvim_open_win(buf, true, {
-			relative = "editor",
-			width = 80,
-			height = 20,
-			col = 10,
-			row = 5,
-			border = "rounded",
-		})
 	end
+	-- Create a new buffer and set it to display the output
+	local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
+	vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result, "\n")) -- Set the buffer lines
+
+	-- Open a new window with the buffer
+	vim.api.nvim_open_win(buf, true, {
+		relative = "editor",
+		width = 80,
+		height = 20,
+		col = 10,
+		row = 5,
+		border = "rounded",
+	})
 end
 
 function M.setup()
