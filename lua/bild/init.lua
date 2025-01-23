8bbbbb4ecf8fbbfe72385ca4143a7c841b97329f
keks137 <guntzjonas@gmail.com>
2025-01-23 22:06:25 +0100

    ls?

--- lua/bild/init.lua
+++ lua/bild/init.lua
@@ -1,9 +1,37 @@
 local M = {}
 
 function M.build()
-	local b = vim.system({ "ls" }, { text = true })
-	print(b)
+	vim.system({ "ls" }, { text = true })
 	print("buildy build")
+	-- Execute the 'ls' command
+	local handle = io.popen("ls -l") -- You can customize the command here
+	if handle == nil then
+		print("whoopsie")
+	else
+		local result = handle:read("*a") -- Read the output
+
+		if result == nil then
+			print("whoopsie")
+		end
+		handle:close()
+
+		if handle == nil then
+			print("whoopsie")
+		end
+		-- Create a new buffer and set it to display the output
+		local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
+		vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result, "\n")) -- Set the buffer lines
+
+		-- Open a new window with the buffer
+		vim.api.nvim_open_win(buf, true, {
+			relative = "editor",
+			width = 80,
+			height = 20,
+			col = 10,
+			row = 5,
+			border = "rounded",
+		})
+	end
 end
 
 function M.setup()
