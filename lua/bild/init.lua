63fa4adbe96a31a0d137a5891b68a12220394c81
keks137 <guntzjonas@gmail.com>
2025-01-23 22:19:05 +0100

    terminal buffer

--- lua/bild/init.lua
+++ lua/bild/init.lua
@@ -3,31 +3,21 @@ local M = {}
 function M.build()
 	vim.system({ "ls" }, { text = true })
 	print("buildy build")
-	-- Execute the 'ls' command
-	local handle = io.popen("make") -- You can customize the command here
-	local result = handle:read("*a") -- Read the output
+	-- Create a new terminal buffer
+	local term_buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
+	vim.api.nvim_open_term(term_buf, {}) -- Open a terminal in the buffer
 
-	if result == nil then
-		print("whoopsie")
-	end
-	handle:close()
-
-	if handle == nil then
-		print("whoopsie")
-	end
-	-- Create a new buffer and set it to display the output
-	local buf = vim.api.nvim_create_buf(false, true) -- Create a new buffer
-	vim.api.nvim_buf_set_lines(buf, 0, -1, false, vim.split(result, "\n")) -- Set the buffer lines
-
-	-- Open a new window with the buffer
-	vim.api.nvim_open_win(buf, true, {
-		relative = "editor",
-		width = 80,
-		height = 20,
-		col = 10,
-		row = 5,
-		border = "rounded",
+	-- Run the 'ls' command in the terminal
+	vim.fn.termopen("ls -l", {
+		on_exit = function(_, exit_code)
+			if exit_code ~= 0 then
+				print("Command exited with code: " .. exit_code)
+			end
+		end,
 	})
+
+	-- Optionally, you can set the buffer to be displayed in a specific window
+	vim.api.nvim_set_current_buf(term_buf)
 end
 
 function M.setup()
