3289a02e5190d95e9683e092a34b03cc7ee874ba
keks137 <guntzjonas@gmail.com>
2025-01-23 22:35:28 +0100

    see

--- lua/bild/init.lua
+++ lua/bild/init.lua
@@ -5,13 +5,13 @@ function M.build()
 	local term_buf = vim.api.nvim_create_buf(false, true)
 	vim.api.nvim_open_term(term_buf, {})
 
-	--	vim.fn.termopen("make", {
-	--		on_exit = function(_, exit_code)
-	--			if exit_code ~= 0 then
-	--				print("Command exited with code: " .. exit_code)
-	--			end
-	--		end,
-	--	})
+	vim.fn.termopen("make", {
+		on_exit = function(_, exit_code)
+			if exit_code ~= 0 then
+				print("Command exited with code: " .. exit_code)
+			end
+		end,
+	})
 
 	--vim.cmd("split")
 	--vim.api.nvim_set_current_buf(term_buf)
