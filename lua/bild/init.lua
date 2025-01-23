5cc340fa5fed23173d73e365e29989118062a84f
keks137 <guntzjonas@gmail.com>
2025-01-23 22:40:51 +0100

    probably not

--- lua/bild/init.lua
+++ lua/bild/init.lua
@@ -3,15 +3,18 @@ local M = {}
 function M.build()
 	print("buildy build")
 	local term_buf = vim.api.nvim_create_buf(false, true)
+	local term_win = vim.api.nvim_open_win(term_buf, true, {
+		relative = "editor",
+		width = 80,
+		height = 20,
+		col = math.floor((vim.o.columns - 80) / 2),
+		row = math.floor((vim.o.lines - 20) / 2),
+		border = "rounded",
+	})
 	vim.api.nvim_open_term(term_buf, {})
 
-	vim.fn.termopen("make", {
-		on_exit = function(_, exit_code)
-			if exit_code ~= 0 then
-				print("Command exited with code: " .. exit_code)
-			end
-		end,
-	})
+	vim.fn.termopen("make", {})
+	vim.api.nvim_set_option_value("winhighlight", "Normal:Normal", { scope = "local" })
 
 	--vim.cmd("split")
 	--vim.api.nvim_set_current_buf(term_buf)
