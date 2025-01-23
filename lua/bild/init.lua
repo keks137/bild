9c5ac99dd10d8423270ec35755ecd4cef32ddb0f
keks137 <guntzjonas@gmail.com>
2025-01-23 22:30:55 +0100

    ????

--- lua/bild/init.lua
+++ lua/bild/init.lua
@@ -2,9 +2,8 @@ local M = {}
 
 function M.build()
 	print("buildy build")
-	local term_buf = vim.api.nvim_create_buf(false, true)
-	vim.api.nvim_open_term(term_buf, {})
-	vim.api.nvim_set_current_buf(term_buf)
+	--local term_buf = vim.api.nvim_create_buf(false, true)
+	--vim.api.nvim_open_term(term_buf, {})
 
 	vim.fn.termopen("make", {
 		on_exit = function(_, exit_code)
@@ -15,6 +14,7 @@ function M.build()
 	})
 
 	vim.cmd("split")
+	--vim.api.nvim_set_current_buf(term_buf)
 end
 
 function M.setup()
